bbeca565b715f64076f037f9865470c972545d5c
Hanif Fathullah <hanif@g2academy.co>
2021-05-06 11:57:31 +0700

    add custom search

--- src/main/java/com/juaracoding/perpustakaan/controller/BukuController.java
+++ src/main/java/com/juaracoding/perpustakaan/controller/BukuController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +19,22 @@ import com.juaracoding.perpustakaan.repository.BukuRepository;
 public class BukuController {
 	@Autowired
 	BukuRepository bukuRepo;
-	
+
 	@GetMapping("/")
-	public List<Buku> getAll(){
+	public List<Buku> getAll() {
 		return bukuRepo.findAll();
 	}
-	
+
+	@GetMapping("/{title}")
+	public List<Buku> getAllByJudulBuku(@PathVariable String title) {
+		System.out.println(title);
+		return bukuRepo.findByJudulBuku(title);
+	}
+
 	@PostMapping("/addBuku")
 	public String addBuku(@RequestBody Buku buku) {
 		bukuRepo.save(buku);
 		return "Insert Berhasil";
 	}
+
 }
--- src/main/java/com/juaracoding/perpustakaan/repository/BukuRepository.java
+++ src/main/java/com/juaracoding/perpustakaan/repository/BukuRepository.java
@@ -1,10 +1,14 @@
 package com.juaracoding.perpustakaan.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.juaracoding.perpustakaan.entity.Buku;
 
 public interface BukuRepository extends JpaRepository<Buku, Long>{
-	
+	@Query(value = "select * from buku where judul_buku LIKE %?1%",nativeQuery=true)
+	List<Buku> findByJudulBuku(String judulBuku);
 }
